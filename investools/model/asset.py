d1991c4d404aa60ecc8d06a7e48c06d6a9f8aede
Alex Thompson <thompson.p.alex@gmail.com>
2022-06-23 21:31:11 -0400

    Remove AssetLocale in favor of flattened, exhaustive AssetClass enum

--- investools/model/asset.py
+++ investools/model/asset.py
@@ -9,27 +9,26 @@ from .base import BaseModel
 class AssetClass(enum.Enum):
 
     CASH = "Cash"
-    EQUITY = "Equity"
-    FIXED_INCOME = "FixedIncome"
-    REAL_ESTATE = "RealEstate"
-
-
-class AssetLocale(enum.Enum):
-
-    US = "US"
-    INTERNATIONAL = "International"
+    EQUITY_US = "Equity_US"
+    EQUITY_US_LARGE_CAP = "Equity_US_Large_Cap"
+    EQUITY_US_MID_CAP = "Equity_US_Mid_Cap"
+    EQUITY_US_SMALL_CAP = "Equity_US_Small_Cap"
+    EQUITY_INTERNATIONAL = "Equity_International"
+    EQUITY_INTERNATIONAL_DEVELOPED = "Equity_International_Developed"
+    EQUITY_INTERNATIONAL_EMERGING = "Equity_International_Emerging"
+    FIXED_INCOME = "Fixed_Income"
+    REIT = "REIT"
 
 
 class Asset(BaseModel):
 
     ticker: str
     class_: AssetClass
-    locale: t.Optional[AssetLocale] = None
     share_price: float
     shares_outstanding: int
     qdi: float = pydantic.Field(1.0, ge=0.0, le=1.0, alias="QDI")
 
-    @pydantic.validator("locale", "qdi", pre=True)
+    @pydantic.validator("qdi", pre=True)
     def _empty_string_as_default(
         cls, value: t.Optional[str], field: pydantic.fields.ModelField
     ) -> t.Any:
